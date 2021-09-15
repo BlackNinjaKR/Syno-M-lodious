aa8b2cbb5f3e8205e9e329c1f1705628b62ae932
BlackNinjaKR <86487321+BlackNinjaKR@users.noreply.github.com>
2021-09-15 06:33:57 +0530

    Add files via upload

---
+++ commands/skip.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "skip",
+    aliases: ["next"],
+    inVoiceChannel: true,
+    run: async (client, message, args) => {
+        const queue = client.distube.getQueue(message)
+        if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        try {
+            client.distube.skip(message)
+            message.channel.send(`${client.emotes.success} | Skipped! Now playing:\n${queue.songs[0].name}`)
+        } catch (e) {
+            message.channel.send(`${client.emotes.error} | ${e}`)
+        }
+    }
+}
